cf040b8722d3e954d5d492014f2cdd8b3d287f29
Mike Choinoski <mrchoinoski@madisoncollege.edu>
2018-04-01 20:02:02 -0500

    Start of Topic Additions 2

--- src/main/java/com/lunchcrunch/controller/TopicApi.java
+++ src/main/java/com/lunchcrunch/controller/TopicApi.java
@@ -3,10 +3,7 @@ package com.lunchcrunch.controller;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.lunchcrunch.entity.Appointment;
-import com.lunchcrunch.entity.Location;
 import com.lunchcrunch.entity.Topic;
-import com.lunchcrunch.entity.User;
 import com.lunchcrunch.persistence.GenericDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,22 +11,12 @@ import org.apache.logging.log4j.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.List;
 
-
-@Path("/topics")
 public class TopicApi {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
-
-    // The Java method will process HTTP GET requests
-    @GET
-    // The Java method will produce content identified by the MIME Media type "json"
-    @Produces("application/json")
 
     public String getAllTopics() {
 
@@ -60,15 +47,16 @@ public class TopicApi {
 
     }
 
-    public String addAppointment(String description) {
+    public String addTopic(String description) {
 
         GenericDao topicDao     = new GenericDao(Topic.class);
 
         Topic newTopic = new Topic(description);
 
-        int id = topicDao.insert(newTopic);
+        topicDao.insert(newTopic);
 
         return "ok";
+
     }
 
 }
